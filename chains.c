a406d79de33fc292858f2271b265b67386ed97aa
sayed azp <sayedazp@gmail.com>
2023-05-25 00:40:38 +0300

    init

---
+++ chains.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * is_chain - test if current char in buffer is a chain delimeter
+ * @info: the parameter struct
+ * @buf: the char buffer
+ * @j: current position in buf
+ *
+ * Return: 1 if chain delimeter, 0 otherwise
+ */
+
+int is_chain(info_t *info, char *buf, size_t j)
+{
+	if (buf[j] == ';') /* found end of this command */
+	{
+		buf[j] = 0; /* replace semicolon with null */
+		info->cmd_buf_type = CMD_CHAIN;
+	}
+	else
+		return (0);
+
+	return (1);
+}
