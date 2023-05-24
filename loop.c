9cef1e265c66f5d7aa0dd68f206cdd8019bcf90f
sayed azp <sayedazp@gmail.com>
2023-05-25 02:42:20 +0300

    sa

--- loop.c
+++ loop.c
@@ -38,6 +38,50 @@ int shellin(info_t *info, char **av)
 	return (builtin_ret);
 }
 
+/**
+ * hsh - main shell loop
+ * @info: the parameter & return info struct
+ * @av: the argument vector from main()
+ *
+ * Return: 0 on success, 1 on error, or error code
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int builtin_ret = 0;
+
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (1)
+		{
+			if (info->cmd_buf_type == CMD_NORM)
+				_puts("$ ");
+		}
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (r == 0)
+			r = get_input(info);
+        else
+            _putchar('\n');
+		free_info(info, 0);
+	}
+	free_info(info, 1);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
 
 /**
  * find_cmd - finds a command in PATH
